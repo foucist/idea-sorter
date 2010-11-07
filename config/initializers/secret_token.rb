2a154c114b58fc34c44b48e72e4eb49bf319bed1
James Robey <james.robey@gmail.com>
2010-11-07 13:33:37 -0800

    initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omgpop::Application.config.secret_token = '6a4cfe2d513b5e7470b006d7eb37d60d314927c848fc30250f005d0a97af2fee0f64525d801f55ba187b074ed6f0159e4f688d1dd9878d1bb7896812710ff446'
