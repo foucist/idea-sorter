f2a6b2037927570c46d5677422c72afadf936361
James Robey <james.robey@gmail.com>
2010-11-16 16:06:37 -0800

    force uniqueness on idea_ranks join table with index

--- db/migrate/20101110050105_create_idea_ranks.rb
+++ db/migrate/20101110050105_create_idea_ranks.rb
@@ -7,9 +7,12 @@ class CreateIdeaRanks < ActiveRecord::Migration
 
       #t.timestamps
     end
+    #Add a unique index that includes both columns. That will prevent you from inserting a record that contains a duplicate category_id/post_id pair.
+    add_index :idea_ranks, [:criterion_id, :idea_id], :unique => true
   end
 
   def self.down
     drop_table :idea_ranks
+    remove_index :idea_ranks, [:criterion_id, :idea_id], :unique => true
   end
 end
