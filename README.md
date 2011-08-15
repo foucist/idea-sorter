812a8a4d3d28c07c0a1587ddb89f924eb1f62aad
James Robey <james.robey@gmail.com>
2011-08-15 11:20:43 +0700

    Added .gitignore/Gemfile.lock/seeds.rb

---
+++ README.md
@@ -0,0 +1,12 @@
+Idea-Sorter - Amazing Click'n'Drag Decision Matrix Tool
+=======================================================
+
+Uses:
+* Rails 3.0.3
+* Prototype
+
+Features: 
+* input for Criteria & Ideas
+* Criteria is draggable in order of priority
+* Ideas are draggable in order of importance RELATIVE to the active Criteria
+* Final Rankings of ideas
---
+++ db/seeds.rb
@@ -0,0 +1,40 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Daley', :city => cities.first)
+Idea.create({"concept"=>"SRS flashcard web app with fluency-based/precision teaching principles incorporated", "title"=>"Fluency Trainer"})
+
+Criterion.create({"title"=>"How easy is this idea to implement?"})
+
+Criterion.create({"title"=>"How much money making potential does this idea have?"})
+
+Criterion.create({"title"=>"How fun is this idea?"})
+
+Criterion.create({"title"=>"How awesome is this idea?"})
+
+Criterion.create({"title"=>"How long will it take to see a return on this idea?"})
+
+Idea.create({"concept"=>"Ads for publishers that displays and split tests banners/landers automatically for a wide variety affiliate offers. Offers average site publishers higher earnings than adsense/etc.", "title"=>"Ad network platform"})
+
+Idea.create({"concept"=>"Attention management-based todo list web app.", "title"=>"AutoFocus"})
+
+Idea.create({"concept"=>"Just need a bookmarklet that lets me tag bookmarks and goes into a \"read it later\" style list.", "title"=>"Bookmarking web app"})
+
+Idea.create({"concept"=>"Combine impossibility.org & domai.nr & wordnavigator.com & thesaurus support. Show all the .tlds & their costs.. Show combinations off recently registered domain names.\r\n\r\nOR\r\nImagine if people could just watch for new interesting domains name and click to buy!", "title"=>"Domainer site"})
+
+Idea.create({"concept"=>"Platform for getting contract jobs and bringing in contract workers to meet the demand. Scalable system for quality control, quick payouts, etc.", "title"=>"Microsourcer"})
+
+Idea.create({"concept"=>"The ultimate checklist site. Needs to have a proper language/UX design for clear readable parallelism/timeline/etc properties.", "title"=>"Check List"})
+
+Idea.create({"concept"=>"Develop a poster, develop a book, and develop a series of exercise equipment.\r\n\r\nThe ultimate, life-long, 3 minutes a day, exercise program.", "title"=>"Exercise 4 Everyone"})
+
+Idea.create({"concept"=>"Make the best possible, awesomely fast, image viewer/taggable organizer. Web-based. Offer syncing to offline later. Private, encrypted images too?", "title"=>"Web-based Image"})
+
+Idea.create({"concept"=>"Utilize Proper JS to make it easy to view comics online. Side ways view, adjustable width. Dynamically resize images in background. Maybe use node.js or html5?\r\n\r\nExtra features later:\r\nMake it easy for people to edit raws, edit language, etc. Social.", "title"=>"Manga Reader Website"})
+
+Idea.create({"concept"=>"Free POF-style dating site for aborad. DateInAsia.com but way better, more like the paid ones, filipinaheart, worldfriends.tv, etc. Language support. Translation support. Target: Japanese & Chinese girls for white men. What about giving points for surveys and the points can be used for getting more messages", "title"=>"Dating site"})
+
+Idea.create({"concept"=>"First implementation: Bookmarklet that popsup with godaddy coupon codes that the user can try out.  Some or all will be my codes. Second implementation: expand to include all coupon codes out there", "title"=>"Coupo.nr site"})
